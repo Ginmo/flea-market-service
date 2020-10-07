47a0373c41d0de44dcd9baa84144f9a3f2c47f07
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-10-07 20:49:06 +0300

    Update itemSchema

--- db_models/items.model.js
+++ db_models/items.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 var itemSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: { type: String, required: false },
+    description: { type: String, required: true },
     category: { type: String, required: true },
     location: { type: String, required: true },
     images: {
