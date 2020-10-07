0226ce5fc3b2e23c7d15b2273d467ffd82284526
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-10-07 14:51:13 +0300

    Created item routes, no implementations

--- components/items.js
+++ components/items.js
@@ -2,7 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("items API");
+    res.send("List ITEMS based on category, location, startDate, endDate");
+});
+
+router.post('/:itemId', (req, res) => {
+    const itemId = req.params.itemId;
+    if (itemId !== undefined) {
+        res.sendStatus(201);
+    } else {
+        res.status(400).send("Missing ID of item");
+    }
+});
+
+router.delete('/:itemId', (req, res) => {
+    const itemId = req.params.itemId;
+    if (itemId !== undefined) {
+        res.sendStatus(201);
+    } else {
+        res.status(400).send("Missing ID of item");
+    }
 });
 
 module.exports = router;
