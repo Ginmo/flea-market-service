2f1ca1d50d05181820a61dc319d2f33b4053b657
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-10-07 20:48:07 +0300

    Update itemSchema

--- db_models/items.model.js
+++ db_models/items.model.js
@@ -14,13 +14,7 @@ var itemSchema = new mongoose.Schema({
     price: { type: Number, required: true },
     date: { type: String, required: true },
     deliveryType: { type: String, required: true },
-    contactInformation: {
-        username: { type: String, required: true },
-        firstname: { type: String, required: true },
-        lastname: { type: String, required: true },
-        email: { type: String, required: true },
-        phonenumber: { type: String, required: true }
-    }
+    user_id: { type: String, required: true }
 });
 
 mongoose.model('Item', itemSchema);
