09cb5c243ded38e2af1872403c26d2de75c09183
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-10-11 16:46:00 +0300

    Implemented search endpoint

--- components/search.js
+++ components/search.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
+const utils = require('../utils');
 
-router.get('/', (req, res) => {
-    res.send("search");
+router.get('/', async (req, res) => {
+    const category = req.query.category;
+    const location = req.query.location;
+    const startDate = req.query.startDate;
+    const endDate = req.query.endDate;
+
+
+    if (startDate !== undefined) {
+        if (moment(startDate, 'YYYY-MM-DD', true).isValid() !== true) {
+            res.status(400).send({ message: "Use format YYYY-MM-DD in search." });
+            return;
+        }
+    }
+    if (endDate !== undefined) {
+        if (moment(endDate, 'YYYY-MM-DD', true).isValid() !== true) {
+            res.status(400).send({ message: "Use format YYYY-MM-DD in search." });
+            return;
+        }
+    }
+
+    try {
+        result = await utils.findItems(category, location, startDate, endDate);
+        if (result.length < 1) {
+            res.status(404).send({ message: "No matches with given parameters." });
+        } else {
+            res.send(result);
+        }
+    } catch (error) {
+        res.status(500).send({ message: "Error while trying to find items." })
+    }
 });
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -2,9 +2,11 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 require('./database');
 const User = mongoose.model('User');
+const Item = mongoose.model('Item');
 
 // 123: $2y$12$0WKx/VjO1eCvjhkeHFhx..seS/3GI.1N17b7VyEc5900lpMA/4aou
 
+let items = [];
 const utils = {
     checkUserLogin: (username, password) => {
         return new Promise(function (resolve, reject) {
@@ -23,6 +25,260 @@ const utils = {
             });
         });
     },
+    findItems: (category, location, startDate, endDate) => {
+        return new Promise(function (resolve, reject) {
+            items = [];
+            if (category !== undefined && location !== undefined) {
+                if (startDate !== undefined || endDate !== undefined) {
+                    findItemByCategoryAndLocationAndDate(category, location, startDate, endDate).then(results => {
+                        resolve(results);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                } else {
+                    findItemByCategoryAndLocation(category, location).then(results => {
+                        resolve(results);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                }
+            }
+
+            else if (category !== undefined && location === undefined) {
+                if (startDate !== undefined || endDate !== undefined) {
+                    findItemByCategoryAndDate(category, startDate, endDate).then(results => {
+                        resolve(results);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                } else {
+                    findItemByCategory(category).then(results => {
+                        resolve(results);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                }
+            }
+
+            else if (category === undefined && location !== undefined) {
+                if (startDate !== undefined || endDate !== undefined) {
+                    findItemByLocationAndDate(location, startDate, endDate).then(results => {
+                        resolve(results);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                } else {
+                    findItemByLocation(location).then(results => {
+                        resolve(results);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                }
+            }
+
+            else if (category === undefined && location === undefined) {
+                if (startDate !== undefined || endDate !== undefined) {
+                    findItemByDate(startDate, endDate).then(results => {
+                        resolve(results);
+                    }).catch(error => {
+                        reject(error);
+                    });
+                } else {
+                    Item.find()
+                        .then(data => {
+                            resolve(data);
+                        }).catch(error => {
+                            reject(error);
+                        });
+
+                }
+            }
+
+
+
+        });
+    }
+
+}
+
+
+// Find items functions
+function findItemByCategory(category) {
+    return new Promise(function (resolve, reject) {
+        Item.find()
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    if (category === data[i].category) {
+                        items.push(data[i]);
+                    }
+                }
+                resolve(items);
+            }).catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function findItemByLocation(location) {
+    return new Promise(function (resolve, reject) {
+        Item.find()
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    if (location === data[i].location) {
+                        items.push(data[i]);
+                    }
+                }
+                resolve(items);
+            }).catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function findItemByDate(startDate, endDate) {
+    return new Promise(function (resolve, reject) {
+        const startDateToEpoch = Date.parse(startDate);
+        const endDateToEpoch = Date.parse(endDate);
+        Item.find()
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    const date = data[i].date.split(" ");
+                    searchedDateToEpoch = Date.parse(date[0]);
+                    if (startDate !== undefined && endDate !== undefined) {
+                        if (searchedDateToEpoch >= startDateToEpoch && searchedDateToEpoch <= endDateToEpoch) {
+                            items.push(data[i]);
+                        }
+                    }
+                    else if (startDate !== undefined && endDate === undefined) {
+                        if (searchedDateToEpoch >= startDateToEpoch) {
+                            items.push(data[i]);
+                        }
+
+                    }
+                    else if (startDate === undefined && endDate !== undefined) {
+                        if (searchedDateToEpoch <= endDateToEpoch) {
+                            items.push(data[i]);
+                        }
+                    }
+                }
+                resolve(items);
+            }).catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function findItemByCategoryAndLocation(category, location) {
+    return new Promise(function (resolve, reject) {
+        Item.find()
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    if (category === data[i].category && location === data[i].location) {
+                        items.push(data[i]);
+                    }
+                }
+                resolve(items);
+            }).catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function findItemByCategoryAndLocationAndDate(category, location, startDate, endDate) {
+    return new Promise(function (resolve, reject) {
+        const startDateToEpoch = Date.parse(startDate);
+        const endDateToEpoch = Date.parse(endDate);
+        Item.find()
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    const date = data[i].date.split(" ");
+                    searchedDateToEpoch = Date.parse(date[0]);
+                    if (startDate !== undefined && endDate !== undefined) {
+                        if (searchedDateToEpoch >= startDateToEpoch && searchedDateToEpoch <= endDateToEpoch && category === data[i].category && location === data[i].location) {
+                            items.push(data[i]);
+                        }
+                    }
+                    else if (startDate !== undefined && endDate === undefined && category === data[i].category && location === data[i].location) {
+                        if (searchedDateToEpoch >= startDateToEpoch) {
+                            items.push(data[i]);
+                        }
+
+                    }
+                    else if (startDate === undefined && endDate !== undefined && category === data[i].category && location === data[i].location) {
+                        if (searchedDateToEpoch <= endDateToEpoch) {
+                            items.push(data[i]);
+                        }
+                    }
+                }
+                resolve(items);
+            }).catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function findItemByCategoryAndDate(category, startDate, endDate) {
+    return new Promise(function (resolve, reject) {
+        const startDateToEpoch = Date.parse(startDate);
+        const endDateToEpoch = Date.parse(endDate);
+        Item.find()
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    const date = data[i].date.split(" ");
+                    searchedDateToEpoch = Date.parse(date[0]);
+                    if (startDate !== undefined && endDate !== undefined) {
+                        if (searchedDateToEpoch >= startDateToEpoch && searchedDateToEpoch <= endDateToEpoch && category === data[i].category) {
+                            items.push(data[i]);
+                        }
+                    }
+                    else if (startDate !== undefined && endDate === undefined && category === data[i].category) {
+                        if (searchedDateToEpoch >= startDateToEpoch) {
+                            items.push(data[i]);
+                        }
+                    }
+                    else if (startDate === undefined && endDate !== undefined && category === data[i].category) {
+                        if (searchedDateToEpoch <= endDateToEpoch) {
+                            items.push(data[i]);
+                        }
+                    }
+                }
+                resolve(items);
+            }).catch(error => {
+                reject(error);
+            });
+    });
+}
+
+function findItemByLocationAndDate(location, startDate, endDate) {
+    return new Promise(function (resolve, reject) {
+        const startDateToEpoch = Date.parse(startDate);
+        const endDateToEpoch = Date.parse(endDate);
+        Item.find()
+            .then(data => {
+                for (let i = 0; i < data.length; i++) {
+                    const date = data[i].date.split(" ");
+                    searchedDateToEpoch = Date.parse(date[0]);
+                    if (startDate !== undefined && endDate !== undefined) {
+                        if (searchedDateToEpoch >= startDateToEpoch && searchedDateToEpoch <= endDateToEpoch && location === data[i].location) {
+                            items.push(data[i]);
+                        }
+                    }
+                    else if (startDate !== undefined && endDate === undefined && location === data[i].location) {
+                        if (searchedDateToEpoch >= startDateToEpoch) {
+                            items.push(data[i]);
+                        }
+                    }
+                    else if (startDate === undefined && endDate !== undefined && location === data[i].location) {
+                        if (searchedDateToEpoch <= endDateToEpoch) {
+                            items.push(data[i]);
+                        }
+                    }
+                }
+                resolve(items);
+            }).catch(error => {
+                reject(error);
+            });
+    });
 }
 
 module.exports = utils;
