c01d2b300581dee2ce7c2301e48ddffe7322cc56
Kimmo Juusola <c8juki00@students.oamk.fi>
2020-10-13 13:40:57 +0300

    Remove unused import

--- components/register.js
+++ components/register.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const utils = require('../utils');
 const { getName } = require('country-list');
 const PhoneNumber = require('awesome-phonenumber');
 const mongoose = require('mongoose');
